ec76ac6bf2d61fe2388c7ab51c9b7ac26e094acd
ian <better.imhs@gmail.com>
2017-09-10 02:10:52 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'components';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>Test</Button>
+      </div>
+    );
+  }
+};
+
+export default App;
